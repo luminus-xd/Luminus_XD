8a7ca9a285b40799c556ce4f533c1701b343d4ad
eleline <eleline.dev@gmail.com>
2023-07-23 17:10:50 +0900

    sitemap構築

---
+++ app/sitemap.ts
@@ -0,0 +1,23 @@
+import { getList, getTagList } from '@/libs/microcms';
+
+export default async function sitemap() {
+  const allPosts = await getList();
+  const allTags = await getTagList();
+
+  const posts = allPosts.contents.map((post) => ({
+    url: `https://luminus.dev/articles/${post.id}`,
+    lastModified: post.createdAt,
+  }));
+
+  const tags = allTags.contents.map((tag) => ({
+    url: `https://luminus.dev/tags/${tag.id}`,
+    lastModified: tag.createdAt,
+  }));
+
+  const routes = ['', '/search'].map((route) => ({
+    url: `https://luminus.dev${route}`,
+    lastModified: new Date().toISOString(),
+  }));
+
+  return [...routes, ...tags, ...posts];
+}
